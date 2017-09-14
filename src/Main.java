aa31dce8e72f5ccd6a5761134e50539cd6bf4eba
yuehtungweng <to880619@gmail.com>
2017-09-14 14:43:23 +0800

    2

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.printf("%s\n%s\n%s\n%s\n%s",
+                "學號:106022020","系級:行動商務與多媒體應用學系一年級","姓名:翁岳桐","時間:2017/09/14","地點:\"I319\"");
+    }
+}
